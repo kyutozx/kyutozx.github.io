f016ba30145f3cf741cb685aad48d548cd7440df
ASUS TUF GAMING <ASUS TUF GAMING@DESKTOP-1G5SLLB.local>
2022-06-18 22:09:47 +0700

    Add animation

--- dist/script.js
+++ dist/script.js
@@ -62,3 +62,24 @@ window.onscroll = function () {
     darkToggle.checked = false;
     toggleDark.classList.remove('hidden');
   }
+
+  // scroll
+  const callback = function (entries) {
+    entries.forEach((entry) => {
+      console.log(entry);
+  
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate-fadeIn");
+      } else {
+        entry.target.classList.remove("animate-fadeIn");
+      }
+    });
+  };
+  
+  const observer = new IntersectionObserver(callback);
+  
+  const targets = document.querySelectorAll(".js-show-on-scroll");
+  targets.forEach(function (target) {
+    target.classList.add("opacity-0");
+    observer.observe(target);
+  });
